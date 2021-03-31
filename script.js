639ffb74e14ee20831822c2c4f13533e76c4c261
ionutgit <72613142+ionutgit@users.noreply.github.com>
2021-03-31 14:35:02 +0300

    add Random Colors Button
    The colour inputs match the background generated on the first page load. 
    Display the initial CSS linear gradient property on page load.
    Random button which generates two random numbers for the colour inputs.

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+
+
+function setText() {
+	css.textContent = body.style.background + ";";
+}
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right,"
@@ -10,13 +17,29 @@ function setGradient() {
 	+ color2.value
 	+ ")";
 
-	css.textContent = body.style.background + ";";
+	setText();
 }
 
-color1.addEventListener('input', function(){
-		setGradient();
-});
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-color2.addEventListener('input', function(){
-		setGradient();
-});
+function setRandomGradient() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+	setText();
+}
+
+setGradient();
+
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
+
+button.addEventListener("click", setRandomGradient);
